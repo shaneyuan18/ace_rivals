8bd78c21630d062a6fed9f5ce59279faec0811cf
Shane Yuan <shaneyuan18@gmail.com>
2018-08-27 13:32:46 -0700

    update sign up with picker

--- src/components/Screens/SignUpScreen.js
+++ src/components/Screens/SignUpScreen.js
@@ -1,20 +1,41 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Picker} from 'react-native';
 import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Input from './Input';
 // import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 
+const NTPRratings = [
+  { name: 'NTRP 1.0', value: 1, },
+  { name: 'NTRP 1.5', value: 1.5, },
+  { name: 'NTRP 2.0', value: 2, },
+  { name: 'NTRP 2.5', value: 2.5, },
+  { name: 'NTRP 3.0', value: 3, },
+  { name: 'NTRP 3.5', value: 3.5, },
+  { name: 'NTRP 4.0', value: 4, },
+  { name: 'NTRP 4.5', value: 4.5, },
+  { name: 'NTRP 5.0', value: 5, },
+  { name: 'NTRP 5.5', value: 5.5, },
+];
 
 class SignUpScreen extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    // };
+    this.state = {
+      rating: 1,
+      langauge:'js',
+    };
   }
 
   componentDidMount() {
 
+  }
+  generateUser() {
+    return {
+      id: 1,
+      name: 'Shane Yuan',
+      rating: '4.0',
+    }
   }
   render() {
     return (
@@ -29,9 +50,22 @@ class SignUpScreen extends React.Component {
         <Input label={'Address'}
           placeholder={'Street 1'}
         />
+        <Picker
+          selectedValue={this.state.rating}
+          style={{ height: 50, width: 1000, margin: 12 }}
+          onValueChange={(itemValue, itemIndex) => this.setState({rating: itemValue})}>
+          {NTPRratings.map((rating) => {
+            return (
+              <Picker.Item label={rating.name} value={rating.value} />
+            )
+          })}
+        </Picker>
         <Button
           title="Sign Up"
-          onPress={() => this.props.navigation.navigate('Home')}
+          onPress={() => {
+            const user = this.generateUser();
+            this.props.navigation.navigate('Home', { user })
+          }}
         />
       </View>
     );
